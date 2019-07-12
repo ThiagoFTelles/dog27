e34ee5d25a1e1e24f857e520cb10d6c467744016
ThiagoFTelles <atendimento.telles@gmail.com>
2019-07-11 22:19:38 -0300

    commit inicial de teste

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production' ?
+//     '/dog27' : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ?
-    '/dog27' : '/'
-}
+  publicPath: "/dog27"
+};
